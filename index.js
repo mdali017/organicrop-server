08546c42a3c3e696783a93e518bf4ad6beb33c69
mdali017 <mohabbatalit8@gmail.com>
2024-02-02 08:31:55 +0600

    Basic Set Up Project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
