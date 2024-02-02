293b4ca8401f65d042170181d0b7f0e3b9e3d207
mdali017 <mohabbatalit8@gmail.com>
2024-02-02 19:58:31 +0600

    vercel error

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const connectDatabase = require("./config/db");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credential: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
